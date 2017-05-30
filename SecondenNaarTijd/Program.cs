c6b9c1eff0d7a96aa2a2d207f6130d5e525d27ff
timour.meeusen <timour.meeusen@B209-11>
2017-05-30 13:20:52 +0200

    struct UMSTijd

--- SecondenNaarTijd/Program.cs
+++ SecondenNaarTijd/Program.cs
@@ -10,12 +10,12 @@ namespace SecondenNaarTijd
     {
         public int h;
         public int m;
-        public int s;
+        public int s;   
     }
 
     class Program
     {
-        private static void SecNaarUrenMinutenSeconden(int totaalSeconden, out int uren, out int minuten, out int seconden)
+        private static void SecNaarUrenMinutenSeconden(int totaalSeconden, out int uren, out int minuten, out int seconden) //output-parameters vermijden: ze zijn lelijk
         {
             //tijdelijke tussentijdse variabele
             int rest = 0;
@@ -27,14 +27,30 @@ namespace SecondenNaarTijd
             seconden = rest % 60;
         }
 
+        private static UMSTijd SecNaarUMS(int totaalSeconden)
+        {
+            UMSTijd nieuweTijd;
+            int rest = 0;
+
+            nieuweTijd.h = totaalSeconden / 3600;
+            rest = totaalSeconden % 3600;
+            nieuweTijd.m = rest / 60;
+            nieuweTijd.s = rest % 60;
+
+            return nieuweTijd;
+        }
+
         static void Main(string[] args)
         {
-            int h = 0;
-            int m = 0;
-            int s = 0;
+            //int h = 0;
+            //int m = 0;
+            //int s = 0;
 
-            SecNaarUrenMinutenSeconden(61, out h, out m, out s);
-            Console.WriteLine("{0} hours; {1} minutes; {2} seconds", h, m, s);
+            //SecNaarUrenMinutenSeconden(61, out h, out m, out s);
+            //Console.WriteLine("{0} hours; {1} minutes; {2} seconds", h, m, s);
+
+            var ums = SecNaarUMS(61);
+            Console.WriteLine("{0} u(u)r(en); {1} minu(u)t(en); {2} second(en)", ums.h, ums.m, ums.s);
         }
     }
 }
